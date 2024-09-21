563eadfe764fe07ebc19e22725a0a69904e3e29c
salcedof <salcedomarcof91@gmail.com>
2024-09-21 07:13:02 -0600

    finished preb 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+       Console.Write("what is your first name? ");
+       string name = Console.ReadLine();
+       
+       Console.Write("what is your last name? ");
+       string last_name = Console.ReadLine();
+
+       Console.WriteLine($"your name is {last_name}, {name} {last_name}.");
+
+
+
+        
     }
 }
