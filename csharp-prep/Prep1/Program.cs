bc5b2eef7fd950915cc3e09d87f0253fcd6ee79d
salcedof <salcedomarcof91@gmail.com>
2024-09-21 07:25:42 -0600

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+       //getting imputs from users// 
        Console.Write("what is your first name? ");
        string name = Console.ReadLine();
-       
+
        Console.Write("what is your last name? ");
        string last_name = Console.ReadLine();
 
