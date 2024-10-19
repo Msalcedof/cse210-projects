d0a2d14911411c5036f2a5627041cd67a8af1017
salcedof <salcedomarcof91@gmail.com>
2024-10-18 20:18:06 -0600

    finished learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,87 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("roberto Rodriguez", "Fractions", "7.3","8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Watters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+    }
+
+
+}
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName =  studentName;
+        _topic = topic;    
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " " + _topic;
+
+    }
+}
+
+public class MathAssignment : Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems)
+    : base(studentName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeWorkList()
+    {
+        return $"section {_textBookSection} Problems{_problems}";
+    }
+
+}
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic , string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        {
+            return $"{_title} by {studentName}";
+        }
     }
 }
